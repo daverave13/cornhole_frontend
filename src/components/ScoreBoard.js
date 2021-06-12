a32cea41a24ab02fd5613a4a7f6d6bd757c27082
David Slusser <dslusser13@gmail.com>
2021-06-11 19:18:46 -0700

    a few aesthitics

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import './scoreBoard.css';
-import EditBoard from './EditBoard';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./scoreBoard.css";
+import EditBoard from "./EditBoard";
 
 const Scoreboard = (props) => {
-
-    const [ gameState, setGameState] = useState({});
-    const [ roundScoreA, setRoundScoreA ] = useState(0);
-    const [ roundScoreB, setRoundScoreB ] = useState(0);
-    const [ editMode, setEditMode ] = useState(false);
-    const [ maxGameId, setMaxGameId ] = useState(-1); 
+    const [gameState, setGameState] = useState({});
+    const [roundScoreA, setRoundScoreA] = useState(0);
+    const [roundScoreB, setRoundScoreB] = useState(0);
+    const [editMode, setEditMode] = useState(false);
+    const [maxGameId, setMaxGameId] = useState(-1);
 
     const getGameState = async () => {
-        // fetch("https://dslusser.com:5000/api/games/", { method: "GET" })
-        //     .then((response) => response.json())
-        //     .then((data) => {
-        //         const currentGame = [...data].filter((game) => game.game_id === props.selectedGameId)[0];
-        //         const mostRecentGame = [...data].sort((a,b) => b.game_id - a.game_id)[0];
-        //         setMaxGameId(mostRecentGame.game_id + 1);
-        //         setGameState(currentGame);
-        //     });
-
-        const response = await fetch("https://dslusser.com:5000/api/games/", { method: "GET" }).then((response) => response.json());
+        const response = await fetch("https://dslusser.com:5000/api/games/", {
+            method: "GET",
+        }).then((response) => response.json());
         // console.log(response);
-        const currentGame = [...response].filter((game) => game.game_id === props.selectedGameId)[0];
-        const mostRecentGame = [...response].sort((a,b) => b.game_id - a.game_id)[0];
+        const currentGame = [...response].filter(
+            (game) => game.game_id === props.selectedGameId
+        )[0];
+        const mostRecentGame = [...response].sort(
+            (a, b) => b.game_id - a.game_id
+        )[0];
         setMaxGameId(mostRecentGame.game_id + 1);
         setGameState(currentGame);
     };
@@ -32,16 +28,16 @@ const Scoreboard = (props) => {
     useEffect(() => {
         getGameState();
     }, [editMode, props.selectedGameId]);
-    
+
     const { game_id, teamA, teamB, scoreA, scoreB, score_to_win } = gameState;
 
     const updateGameState = async () => {
         const newBody = {
-            'teamA': teamA,
-            'teamB':  teamB,
-            'scoreA': scoreA + roundScoreA,
-            'scoreB': scoreB + roundScoreB,
-            'score_to_win': score_to_win,
+            teamA: teamA,
+            teamB: teamB,
+            scoreA: scoreA + roundScoreA,
+            scoreB: scoreB + roundScoreB,
+            score_to_win: score_to_win,
         };
 
         const requestOptions = {
@@ -52,23 +48,24 @@ const Scoreboard = (props) => {
             body: JSON.stringify(newBody),
         };
 
-        await fetch("https://dslusser.com:5000/api/games/" + game_id, requestOptions).then(
-            (response) => response.json());
-        
+        await fetch(
+            "https://dslusser.com:5000/api/games/" + game_id,
+            requestOptions
+        ).then((response) => response.json());
+
         setRoundScoreA(0);
         setRoundScoreB(0);
         setGameState(newBody);
         getGameState();
-    
-    }
+    };
 
     const newGame = () => {
         const body = {
-            'teamA': 'teamA',
-            'teamB': 'teamB',
-            'scoreA': 0,
-            'scoreB': 0,
-            'score_to_win': 21,
+            teamA: "teamA",
+            teamB: "teamB",
+            scoreA: 0,
+            scoreB: 0,
+            score_to_win: 21,
         };
 
         setRoundScoreA(0);
@@ -84,13 +81,14 @@ const Scoreboard = (props) => {
         };
 
         fetch("https://dslusser.com:5000/api/games/", requestOptions).then(
-            (response) => response.json());
+            (response) => response.json()
+        );
 
         getGameState();
-    }
+    };
 
     const increaseScore = (team) => {
-        if (team === 'teamA') {
+        if (team === "teamA") {
             if (roundScoreB > 0) {
                 const newRoundScoreB = roundScoreB - 1;
                 setRoundScoreB(newRoundScoreB);
@@ -107,41 +105,59 @@ const Scoreboard = (props) => {
                 setRoundScoreB(newRoundScoreB);
             }
         }
-    }   
+    };
 
     const onReturnToListClick = () => {
         updateGameState();
         props.setSelectedGameId(-1);
-    }
- 
+    };
+
     return (
-        <div className="score-board">
-            <div className="main-grid">
-                <div className="top-row right-border"></div>
-                <div className="top-row right-border">{ teamA }</div>
-                <div className="top-row">{ teamB }</div>
-                <div className="mid-row right-border">Round</div>
-                <div className="mid-row right-border score" onClick={() => increaseScore('teamA')}>{ roundScoreA }</div>
-                <div className="mid-row score" onClick={() => increaseScore('teamB')}>{ roundScoreB }</div>
-                <div className="bot-row right-border">Game</div>
-                <div className="bot-row right-border score" >{ scoreA }</div>
-                <div className="bot-row score">{ scoreB }</div>
+        <div className='score-board'>
+            <div className='main-grid'>
+                <div className='top-row right-border'></div>
+                <div className='top-row right-border'>{teamA}</div>
+                <div className='top-row'>{teamB}</div>
+                <div className='mid-row right-border'>Round</div>
+                <div
+                    className='mid-row right-border score'
+                    onClick={() => increaseScore("teamA")}
+                >
+                    {roundScoreA}
+                </div>
+                <div
+                    className='mid-row score'
+                    onClick={() => increaseScore("teamB")}
+                >
+                    {roundScoreB}
+                </div>
+                <div className='bot-row right-border'>Game</div>
+                <div className='bot-row right-border score'>{scoreA}</div>
+                <div className='bot-row score'>{scoreB}</div>
             </div>
-            <div className="score-to-win">
+            <div className='score-to-win'>
                 <div>Score To Win:&nbsp;&nbsp;&nbsp;</div>
-                <div className="">
-                    <div>{ score_to_win }</div>
+                <div className=''>
+                    <div>{score_to_win}</div>
                 </div>
             </div>
-            <div className="buttons">
+            <div className='buttons'>
                 <button onClick={updateGameState}>End Round</button>
                 <button onClick={() => setEditMode(true)}>Edit Game</button>
                 <button onClick={newGame}>New Game</button>
                 <button onClick={onReturnToListClick}>Return to List</button>
             </div>
-            {editMode ? <EditBoard gameState={gameState} setEditMode={setEditMode} setGameState={setGameState}/> : ""}
+            {editMode ? (
+                <EditBoard
+                    gameState={gameState}
+                    setEditMode={setEditMode}
+                    setGameState={setGameState}
+                />
+            ) : (
+                ""
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Scoreboard;
